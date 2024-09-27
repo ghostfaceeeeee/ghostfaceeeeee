7425438507d5098a8c85b5f8dd2421214ef50cea
whaleafs <asakura1198@gmail.com>
2024-09-27 21:30:06 +0700

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
-Print (Helloguys)
+Print("Helloguys")
 <!---
 ghostfaceeeeee/ghostfaceeeeee is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
